97a30d3fab49f1f10dcdf4da138240851fe7e8fc
Juillermo <grm992@gmail.com>
2017-03-21 21:23:34 +0100

    Añadido método xpath, yay!

--- mundo_spider.py
+++ mundo_spider.py
@@ -8,13 +8,13 @@ class QuotesSpider(scrapy.Spider):
 	yield scrapy.Request(url, self.ini_parse)
 
     def ini_parse(self, response):
-	for href in response.css('li.tab a::attr(href)').extract():
+	for href in response.xpath('//*[@data-section]/a/@href').extract():
             yield scrapy.Request(response.urljoin(href), callback=self.parse)
 
     def parse(self, response):
-        for normal in response.css('h3.mod-title.normal'):
+        for headline in response.xpath('//*[@itemprop="headline"]/a'):
             yield {
-		'section': response.css('title::text').extract_first(),
-                'text': normal.css('a::text').extract_first(),
-		'link': normal.css('a::attr("href")').extract_first(),
+		'section': response.xpath('//title/text()').extract_first(),
+                'text': headline.xpath('./text()').extract_first(),
+		'link': headline.xpath('./@href').extract_first(),
             }
